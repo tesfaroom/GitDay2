033aed7040908cfee5a4dfa4d8a040ef6c6f4271
winta <winta@DESKTOP-G9LOJV9>
2022-08-01 19:35:13 -0400

    Second demo push commit.......

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -5,6 +5,12 @@ public class DemoTest {
 	public static void main(String[] args) {
 
 		System.out.println("Test");
+	}
+		
+		public void pushDemo() {
+			System.out.println(" push demo two");
+		}
+		
 
 	}
-}
+
