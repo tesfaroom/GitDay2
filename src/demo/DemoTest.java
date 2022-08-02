40d51cc7d089d26c6ed5c1f6b10ef1998afc21f6
tesfaroom <tesfaroom@gmail.com>
2022-08-01 20:16:59 -0400

    Update DemoTest.java

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -11,9 +11,8 @@ public class DemoTest {
 	      System.out.println(" push demo two");
 	}
 	 public static void pullCode(){
-	 System.out.println("Pull demo trial already pulled");
 
-	System.out.print(" This is for pull trial from remot repo to local repo to workspace");
+	System.out.print(" This is for pull trial from remot repo to local repo to workspace")
 	 }	
 
 	}
